b7307719eef203de15e78c937582f68547b6b52f
theonlydonwilson <40670397+theonlydonwilson@users.noreply.github.com>
2018-06-28 18:21:01 -0400

    Update exampleDestructure.js

--- exampleDestructure.js
+++ exampleDestructure.js
@@ -1,18 +1,14 @@
-#assuming we recontruct the array first
+const locations = [
+  'Brentwood',
+  'California',
+  'Texas',
+  'Santa Monica',
+  'Oregon',
+  'Washington',
+  'Arizona',
+  'Venice',
+  'Utah'
+];
 
-const locations = {
-  a: 'Brentwood',
-  b: 'California',
-  c: 'Texas',
-  d: 'Santa Monica',
-  e: 'Oregon',
-  f: 'Washington',
-  g: 'Arizona',
-  h: 'Venice',
-  i: 'Utah'
-};
-const { a, d, h } = locations;
-
-const location_one = a;   //Brentwood
-const location_two = d;   //Santa Monica
-const location_three = h; //Venice
+var [location_one, , , location_two, , , ,location_three] = locations;  
+console.log(location_one, location_two, location_three); // 'Brentwood' 'Santa Monica' 'Venice'
